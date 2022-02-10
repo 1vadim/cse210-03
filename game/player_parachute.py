61cb070c7369e971651758a1143cf7a0a305f804
samdevbeltran <samigamedev96@gmail.com>
2022-02-09 23:02:57 -0500

    a few changes were done

--- game/player_parachute.py
+++ game/player_parachute.py
@@ -1,11 +1,8 @@
 import random
 """
 This class is going to evaluate if the inputs player is or not in the chosen word
-    
-    1- you need to create a cunstructor 
-    2- you need to create the attributes:
-    3-you need to create the mothods
-Author: Sam and jonathon 
+
+Author: Sam
 """
 class Player_parachute:
     
